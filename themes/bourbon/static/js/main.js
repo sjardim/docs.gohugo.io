717085543a3733cdf54b4c89f42499ba8ffe5ccb
digitalcraftsman <digitalcraftsman@users.noreply.github.com>
2015-12-28 21:39:48 +0100

    Add back-to-top button

--- themes/bourbon/static/js/main.js
+++ themes/bourbon/static/js/main.js
@@ -145,4 +145,24 @@ jQuery(document).ready(function(){
 		}
 		scrolling = false;
 	}
+
+	// Back-to-top button
+	$(".back-to-top").hide();
+	$(function() {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 150) {
+				$('.back-to-top').fadeIn();
+			} else {
+				$('.back-to-top').fadeOut();
+			}
+		});
+
+		$('.back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+	
 });
